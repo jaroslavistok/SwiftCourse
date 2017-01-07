fb1dfeb8166cfa6e994f16996c5e93a6de238448
Jaroslav Ištok <jaroslav.istok@gmail.com>
2017-01-07 09:53:16 +0100

    added first version of 3rd homework

---
+++ vma-5stars-profile/5stars-starter/RegistrationManager.swift
@@ -0,0 +1,54 @@
+//
+//  User.swift
+//  5stars
+//
+//  Created by Lubos Ilcik on 15/11/2016.
+//  Copyright © 2016 Touch4IT. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+fileprivate let className = "ParseUser"
+fileprivate let userNameKey = "userName"
+
+fileprivate let domain = "com.touch4it.vma"
+let userNameEmptyErrorCode = -5001
+let userNameAlreadyTakenErrorCode = -5002
+
+struct RegistrationManager {
+    
+    let userName: String
+ 
+    func signUp(_ withBlock: @escaping (_ success: Bool, _ error: Error?) -> Void) {
+        
+        guard !userName.isEmpty else {
+            let err = NSError(domain: domain, code: userNameEmptyErrorCode, userInfo: [NSLocalizedDescriptionKey: "User name is empty."])
+            withBlock(false, err)
+            return
+        }
+        
+        let parseUserQuery = PFQuery(className: className).whereKey(userNameKey, equalTo: userName)
+        parseUserQuery.countObjectsInBackground { count, error in
+            
+            if error != nil {
+                withBlock(false, error)
+                return
+            }
+            
+            if count == 0 {
+                self.saveUserName(withBlock)
+            } else {
+                let err = NSError(domain: domain, code: userNameAlreadyTakenErrorCode, userInfo: [NSLocalizedDescriptionKey: "User name already taken."])
+                withBlock(false, err)
+            }
+        }
+    }
+    
+    private func saveUserName(_ block: @escaping (_ success: Bool, _ error: Error?) -> Void) {
+        let parseUser = PFObject(className: className)
+        parseUser[userNameKey] = userName
+        parseUser.saveInBackground (block: block)
+    }
+    
+}
---
+++ vma-5stars-profile/5stars-starter/RegistrationViewController.swift
@@ -0,0 +1,81 @@
+//
+//  RegistrationViewController.swift
+//  5stars-starter
+//
+//  Created by Jaroslav Istok on 08/12/2016.
+//  Copyright © 2016 Touch4IT. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RegistrationViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var informationLabel: UILabel!
+    
+    var activityIndicator = UIActivityIndicatorView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        activityIndicator.center = self.view.center
+        self.view.addSubview(activityIndicator)
+    }
+    
+    
+    @IBAction func hideKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func registerAction(_ sender: UIButton) {
+        
+        let username = userNameTextField.text
+        
+        view.endEditing(true)
+        
+        let parseUser = RegistrationManager(userName: username!)
+        
+        parseUser.signUp() { (_ success: Bool, _ error: Error?) -> Void in
+            self.activityIndicator.startAnimating()
+            if (success) {
+                var stateStorage = StateStorage()
+                stateStorage.isRegistered = true
+                stateStorage.registeredUserName = username
+                self.transitToRegisteredUsersViewController()
+                self.stopActivityIndicator()
+            } else {
+                self.informationLabel.text = error?.localizedDescription
+                self.stopActivityIndicator()
+            }
+        }
+    }
+    
+    private func transitToRegisteredUsersViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let registeredUsersViewController = storyboard.instantiateViewController(withIdentifier: "RegisteredUsersController") as! RegisteredUsersViewController
+        registeredUsersViewController.isPresented = true
+        present(registeredUsersViewController, animated: true, completion: nil)
+    }
+    
+    private func stopActivityIndicator(){
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.stopAnimating()
+    }
+}
+
+extension RegistrationViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 0 {
+            passwordTextField.becomeFirstResponder()
+        } else if textField.tag == 1 {
+            view.endEditing(true)
+        }
+        return true
+    }
+}
